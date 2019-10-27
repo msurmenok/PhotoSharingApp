1302f5539dce6359ea9dde3a1f1ec3239bac261b
Jing Z <faith.zjing@gmail.com>
2019-10-27 12:04:01 -0700

    Update login and add logout

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flask import Flask, render_template
 from flask import Flask, render_template, request, redirect, url_for, session, \
     flash
 from warrant import Cognito
@@ -10,12 +9,18 @@ app.config['SECRET_KEY'] = SECRET_KEY
 u = Cognito(USER_POOL_ID, CLIENT_ID, user_pool_region=REGION)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if session.get('user_login') is True:
+        u = Cognito(USER_POOL_ID, CLIENT_ID, REGION,
+                    id_token=session.get('id_token'),
+                    refresh_token=session.get('refresh_token'),
+                    access_token=session.get('access_token'),
+                    username=session.get('username'))
 
+        return render_template("index.html", username=u.username,
+                               user_login=session.get('user_login'))
 
-@app.route('/signup')
     return render_template('index.html')
 
 
@@ -41,8 +46,26 @@ def signup():
     return render_template("signup.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    # TODO: render to each user
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        u = Cognito(USER_POOL_ID, CLIENT_ID, REGION, username=username,
+                    access_key='dummy_not_used', secret_key='dummy_not_used')
+
+        try:
+            u.authenticate(password)
+            session['asscess_token'] = u.access_token
+            session['refresh_token'] = u.refresh_token
+            session['id_token'] = u.id_token
+            session['user_login'] = True
+            session['username'] = username
+            return redirect(url_for('index'))
+        except ClientError as e:
+            flash(e.response.get('Error').get('Message'))
+
     return render_template("login.html")
 
 
@@ -60,5 +83,19 @@ def confirm_signup(username):
         return redirect(url_for('login', username=username))
 
     return render_template('confirm_signup.html')
+
+
+@app.route('/logout')
+def logout():
+    if session.get('user_login'):
+        session.clear()
+    return render_template('index.html')
+
+
+# @app.route('/confirm_signup')
+# def confirm():
+#     return render_template('confirm_signup.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)

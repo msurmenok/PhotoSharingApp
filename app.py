9c070b05c923a7f062ca7b21737f1c6782cfc099
Jing Z <faith.zjing@gmail.com>
2019-10-27 11:59:13 -0700

    Adding sign up and alert messages
    Add user signup page and function

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, \
+    flash
+from warrant import Cognito
+from settings import *
+from botocore.exceptions import ClientError
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+u = Cognito(USER_POOL_ID, CLIENT_ID, user_pool_region=REGION)
 
 
 @app.route('/')
@@ -9,7 +16,28 @@ def index():
 
 
 @app.route('/signup')
+    return render_template('index.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == "POST":
+
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']  # hashed in warrant
+        password_confirm = request.form['confirm_password']
+        if password != password_confirm:
+            flash("Passwords don't match!")
+            return redirect(url_for('signup'))
+        u.add_base_attributes(email=email)
+        try:
+            u.register(username, password)
+            return redirect(url_for("confirm_signup", username=username,
+                                    password=password))
+        except ClientError as e:
+            flash(e.response.get('Error').get('Message'))
+
     return render_template("signup.html")
 
 
